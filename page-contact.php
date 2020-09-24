51c4388942b258e5690b204e796e3f11dd43f59d
DAISUKE IZUTSU <idutsudaisuke@gmail.com>
2020-09-24 13:04:15 +0900

    remove page-contact.php

--- page-contact.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div class="mmm-post mmm-post--single">
-    <h2 class="mmm-post__title mmm-post--single__title">お問い合わせフォーム</h2>
-    <div class="mmm-post__content mmm-post--single__content">
-        <?php require_once('libs/mmm-form/example.html'); ?>
-    </div>
-</div>
-<?php get_template_part('templates/after-content'); ?>
-<?php get_footer(); ?>
