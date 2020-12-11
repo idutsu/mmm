cde2985bb25e8622be0eab0b8093dd22d997f1b4
DAISUKE IDUTSU <idutsudaisuke@gmail.com>
2020-12-11 12:50:39 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -609,6 +609,15 @@ add_action('pre_get_posts', function($query){
     }
 });
 
+//管理者メールアドレス変更
+remove_action( 'add_option_new_admin_email', 'update_option_new_admin_email' );
+remove_action( 'update_option_new_admin_email', 'update_option_new_admin_email' );
+function wpdocs_update_option_new_admin_email( $old_value, $value ) {
+    update_option( 'admin_email', $value );
+}
+add_action( 'add_option_new_admin_email', 'wpdocs_update_option_new_admin_email', 10, 2 );
+add_action( 'update_option_new_admin_email', 'wpdocs_update_option_new_admin_email', 10, 2 );
+
 
 //スライダー関連
 //include_once('libs/mmm-slider/functions.php');
